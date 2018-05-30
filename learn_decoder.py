4436fd2c1ca6b668cf57325813167a19ec738fc2
Jon Gauthier <jon@gauthiers.net>
2018-05-30 14:08:04 -0400

    convert to encoder regression rather than decoding

--- learn_decoder.py
+++ learn_decoder.py
@@ -23,7 +23,8 @@ def learn_decoder(images, encodings):
       alphas=[1, 10, .01, 100, .001, 1000, .0001, 10000, .00001, 100000, .000001, 1000000, 10000000],
       fit_intercept=False
   )
-  ridge.fit(images, encodings)
+  # ridge.fit(images, encodings)
+  ridge.fit(encodings, images)
   logger.debug("Best alpha: %f", ridge.alpha_)
   return ridge
 
@@ -47,12 +48,12 @@ def iter_folds(subject_data, encodings, n_folds=18):
 
     test_imaging = subject_data[mask]
     test_semantic = encodings[mask]
-    target_semantic_idxs = idxs
+    target_image_idxs = idxs
 
-    yield (train_imaging, train_semantic), (test_imaging, test_semantic, target_semantic_idxs)
+    yield (train_imaging, train_semantic), (test_imaging, test_semantic, target_image_idxs)
 
 
-def eval_ranks(clf, test_imaging, target_semantic_idxs, encodings):
+def eval_ranks(clf, test_semantic, target_imaging_idxs, images):
   """
   Evaluate a trained classifier, predicting the concepts associated with test imaging data.
 
@@ -62,50 +63,50 @@ def eval_ranks(clf, test_imaging, target_semantic_idxs, encodings):
     rank_of_correct: N_test vector indicating the rank of the target concept for each
       test input.
   """
-  N_test = len(test_imaging)
-  assert N_test == len(target_semantic_idxs)
+  N_test = len(test_semantic)
+  assert N_test == len(target_imaging_idxs)
 
   # yields an N_test * D_sem matrix
-  pred_semantic = clf.predict(test_imaging)
+  pred_imaging = clf.predict(test_semantic)
 
   # calculate pairwise similarities with semantic data
   # yields an N_test * C matrix
-  pred_semantic /= np.linalg.norm(pred_semantic, axis=1, keepdims=True)
-  encodings_normed = encodings / np.linalg.norm(encodings, axis=1, keepdims=True)
-  similarities = np.dot(pred_semantic, encodings_normed.T)
+  pred_imaging /= np.linalg.norm(pred_imaging, axis=1, keepdims=True)
+  images_normed = images / np.linalg.norm(images, axis=1, keepdims=True)
+  similarities = np.dot(pred_imaging, images_normed.T)
 
   # Rank similarities row-wise in descending order
   rankings = np.argsort(-similarities, axis=1)
   # Find the rank of the desired concept vector
-  matches = np.equal(rankings, target_semantic_idxs[:, np.newaxis])
+  matches = np.equal(rankings, target_imaging_idxs[:, np.newaxis])
   rank_of_correct = np.argmax(matches, axis=1)
   return rankings, rank_of_correct
 
 
-def run_fold(fold, encodings, permute_targets=False):
+def run_fold(fold, images, permute_targets=False):
   """
   Train and evaluate a classifier on the given CV setup.
 
   Args:
     fold:
-    encodings: N_concepts * encoding_dim matrix
+    images: N_concepts * image_dim matrix
     permute_targets: If `True`, permute the target semantic idxs in order to
       evaluate baseline performance.
 
   Returns:
-    rankings: N_test * N_concepts integer matrix. Each row is a list of concept
+    rankings: N_test * N_images integer matrix. Each row is a list of image
       IDs ranked using the model.
-    rank_of_correct: N_test vector indicating the rank of the target concept
+    rank_of_correct: N_test vector indicating the rank of the target image
       for each test input.
   """
-  (train_imaging, train_semantic), (test_imaging, test_semantic, target_semantic_idxs) = fold
+  (train_imaging, train_semantic), (test_imaging, test_semantic, target_image_idxs) = fold
   clf = learn_decoder(train_imaging, train_semantic)
 
   if permute_targets:
-    target_semantic_idxs = target_semantic_idxs.copy()
-    np.random.shuffle(target_semantic_idxs)
+    target_image_idxs = target_image_idxs.copy()
+    np.random.shuffle(target_image_idxs)
 
-  rankings, rank_of_correct = eval_ranks(clf, test_imaging, target_semantic_idxs, encodings)
+  rankings, rank_of_correct = eval_ranks(clf, test_semantic, target_image_idxs, images)
   return rankings, rank_of_correct
 
 
@@ -149,13 +150,13 @@ def main(args):
 
     folds = iter_folds(subject_images, encodings, n_folds=args.n_folds)
     for i, fold in enumerate(tqdm(folds, total=args.n_folds, desc="%s folds" % subject)):
-      _, rank_of_correct = run_fold(fold, encodings)
+      _, rank_of_correct = run_fold(fold, subject_images)
       perf_test.append(rank_of_correct.mean())
       tqdm.write("Fold %2i:\t\tmin %3.1f\tmean %3.1f\tmed %3.1f\tmax %3.1f" %
                  (i, rank_of_correct.min(), rank_of_correct.mean(),
                   np.median(rank_of_correct), rank_of_correct.max()))
 
-      _, rank_of_correct_permute = run_fold(fold, encodings, permute_targets=True)
+      _, rank_of_correct_permute = run_fold(fold, subject_images, permute_targets=True)
       perf_permute.append(rank_of_correct_permute.mean())
       tqdm.write("Fold permuted %2i:\tmin %3.1f\tmean %3.1f\tmed %3.1f\tmax %3.1f" %
                  (i, rank_of_correct_permute.min(), rank_of_correct_permute.mean(),
