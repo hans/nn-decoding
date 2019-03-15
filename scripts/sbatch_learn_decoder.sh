39a2d75cbf5ca0de42762c312c21473edb7b67c5
Jon Gauthier <jon@gauthiers.net>
2019-03-15 14:06:29 -0400

    update sbatch_learn_decoder for new scripts

--- scripts/sbatch_learn_decoder.sh
+++ scripts/sbatch_learn_decoder.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/bash
 
-#SBATCH --mem=16384
-#SBATCH -t 1-0
-#SBATCH -c 1
+#SBATCH --mem=4G
+#SBATCH -t 0-1
+#SBATCH -c 3
 #SBATCH --qos=cpl
+#SBATCH -a 5-250:5%1
 
 source /etc/profile.d/modules.sh
 source ~/.profile
 source activate decoding
 echo "Learning decoder with encoding $encoding"
-python learn_decoder.py data/stimuli_384sentences.txt data \
-    encodings/384sentences.${encoding}.npy \
+python src/learn_decoder.py data/sentences/stimuli_384sentences.txt data/brains/${brain} \
+    models/bert/${encoding}-${SLURM_ARRAY_TASK_ID}.npy \
     --encoding_project 256 \
-    --out_prefix perf.384sentences.${encoding}
+    --n_jobs ${SLURM_JOB_CPUS_PER_NODE} \
+    --n_folds 8 \
+    --out_prefix models/decoders/${encoding}
